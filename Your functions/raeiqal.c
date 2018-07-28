2b84c730ea4f5122fa91d82c59b43cd5354dcb40
Fixided <fixided@gmail.com>
2018-07-28 23:22:44 +0800

    The base of the game.

---
+++ Your functions/raeiqal.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+struct userInfo{
+  char gamePiece; //X, Y, O
+  int boardPos; //position of player
+  int special; //Ignore this, this is for missing turns and shit.
+};
+
+int dice();
+void buildBoard(struct userInfo currentB[], int roll, int i);
+int move(struct userInfo currentB[], int i);
+
+int main (){
+  //initializing everything
+  struct userInfo player[3]; // player[0] is X, player[1] is Y, player [2] is O
+  player[0].gamePiece = 'X';
+  player[1].gamePiece = 'Y';
+  player[2].gamePiece = 'O';
+
+  for(int i = 0; i < 3 ; i++)
+  {
+    player[i].boardPos = 0;
+  }
+///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///
+  int yourMove = 0; //Who's turn is it?
+  while(move(player, yourMove)){ //pass all players user info
+    yourMove++; //next person's turn
+    if(yourMove == 3) //after the 3rd guy, i want it back to the 1st guy
+      yourMove = 0;
+  }
+///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///DEMO///
+}
+
+int dice(){
+  ///rolling the dice
+  int diceNumb; //this is a LOCAL variable
+  char c;
+  time_t t;
+  srand  ( (unsigned) time(&t));
+
+  //Only allow enter button
+    printf("Press enter to roll the dice\n");
+    c=getchar();
+  while(c != '\n'){
+    while(getchar() != '\n');
+    printf("Press enter to roll the dice\n");
+    c=getchar();
+  }
+
+  //rolling the dice
+  do{
+    diceNumb=rand() % 7;
+    }while(diceNumb == 0);
+
+    printf("Rolling the dice...\n%d", diceNumb);
+    return diceNumb; //number is then returned to whoever called the function
+}
+
+int move(struct userInfo currentB[], int i){
+    int roll, die;
+    roll = die = dice(); //roll and die now have the dice number stored
+    int flag = 0; //this is to check if they are over tile 50 (max tile, so i want to bring them backwards)
+    for(; roll > 0; roll--){
+        if(++currentB[i].boardPos > 50){
+            currentB[i].boardPos--;
+            flag = 1;
+            break;
+        }
+        Sleep(500);
+        system("cls");
+        
+        buildBoard(currentB, die, i);
+    }
+    if(flag == 1){ //Have to go back if you don't land on tile 50 perfectly
+        for(; roll > 0; roll--){
+        Sleep(500);
+        system("cls");
+        buildBoard(currentB, die, i);
+        }
+    }
+    if(currentB[i].boardPos == 50){ //If the guy is on 50 Winner!
+      printf("\t\t\tWinner!");
+      return 0;
+    }
+    else
+      return 1;
+}
+
+void buildBoard(struct userInfo currentB[], int roll, int i){
+    int numb = 0, flow = 0, boardNumb = 60, tile = 0;
+    for(int vert = 0; vert <= 30; vert++, numb++){ //draws the vertical lines
+        if(vert % 6){
+            printf("|");
+        for(int row = 0; row < 10; row++){
+          //variable tile knows what box/square is what number
+          if(flow % 2) //finds the right tile number through an algorithm
+            tile = boardNumb + row; 
+          else
+            tile = boardNumb - row; 
+
+            if(numb == 1){ //printf the tile number
+                if(flow % 2)
+                    printf("\t%8d|", tile);
+                else
+                    printf("\t%8d|", tile);
+            }
+            else if(numb == 2 && currentB[0].boardPos == tile)
+              printf("\t%-8c|", currentB[0].gamePiece);
+            else if(numb == 3 && currentB[1].boardPos == tile)
+              printf("\t%-8c|", currentB[1].gamePiece);
+            else if(numb == 4 && currentB[2].boardPos == tile)
+              printf("\t%-8c|", currentB[2].gamePiece);
+            else
+                printf("\t\t|");
+        }
+        printf("\n");
+    }
+    else{ //prints +-----+-----+
+      for(int horz = 0; horz <= 160; horz++){
+        if(horz % 16){
+          printf("-");
+        }
+        else
+        printf("+");
+      }
+      printf("\n");
+
+      numb=0; //needs to reset for next rows after +-----+-----+
+      flow++;
+
+      //DA ALGORITHM! MUAHAHAHAAHAH!
+      if(flow % 2)
+        boardNumb -= 19; 
+      else
+        boardNumb -= 1;
+    }
+  }
+  printf("%c rolled a %d\n", currentB[i].gamePiece, roll); //prints the dice roll and the guy who rolled it
+}
