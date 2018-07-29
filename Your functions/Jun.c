f63658b752bd527c7466299d2ecf767c39b09543
Jun0424 <41717915+Jun0424@users.noreply.github.com>
2018-07-29 18:36:48 +0800

    I managed to do the whole loop for AI but for the switch case 1 somehow i can't break the function after displaying "YOU WIN" at line 31. It works properly on switch case 2 though

---
+++ Your functions/Jun.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct playerInfo{
+    int Pos;
+};
+
+int botAI()
+{
+    int turn, diceRoll = 5;
+    struct playerInfo X,Y;
+    char c;
+    X.Pos = 0;
+    Y.Pos = 0;
+    printf("Please choose who to play first: \n");
+    printf("Press 1 to play first\n");
+    printf("Press 2 to let computer play first\n");
+    scanf("%d",&turn);
+
+    switch(turn){
+    case 1:
+            do{
+                printf("your turn : \n");
+                printf("Press enter to roll dice: \n");
+                c = getch();
+                if (c ='\n'){
+                printf("%d\n",diceRoll);}
+                X.Pos += diceRoll;
+                printf("Your position: %d\n",X.Pos);
+                if(X.Pos>=100){
+                printf("YOU WIN!!!");
+                break;
+                }
+            else{
+                printf("Computer's turn:\n");
+                printf("Rolling dice...\n");
+                printf("%d\n",diceRoll);
+                Y.Pos += diceRoll;
+                printf("Computer's position: %d\n",Y.Pos);
+            }
+                if (Y.Pos>=100){
+                    printf("YOU LOSE!!!");
+                    break;
+                }else
+                continue;
+            }while ((X.Pos<100)||(Y.Pos<100));
+
+    case 2:
+        do{
+                printf("Computer's turn:\n");
+                printf("Rolling dice...\n");
+                printf("%d\n",diceRoll);
+                Y.Pos += diceRoll;
+                printf("Computer's position: %d\n",Y.Pos);
+                if (Y.Pos>=100){
+                    printf("YOU LOSE!!!");
+                    break;
+                }
+                else {
+                printf("your turn : \n");
+                printf("Press enter to roll dice: \n");
+                c = getch();
+                if (c ='\n'){
+                printf("%d\n",diceRoll);}
+                X.Pos += diceRoll;
+                printf("Your position: %d\n",X.Pos);
+                }
+                if(X.Pos>=100){
+                printf("YOU WIN!!!");
+                break;
+                 }else
+                continue;
+            }while ((X.Pos<100)||(Y.Pos<100));
+    }
+}
+
