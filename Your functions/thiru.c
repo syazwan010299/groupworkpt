749be7ad73a42494670bcb7a541ccc1f3c353c38
Fixided <fixided@gmail.com>
2018-08-01 13:00:02 +0800

    thirus function for safekeeping

---
+++ Your functions/thiru.c
@@ -0,0 +1,38 @@
+int specialtiles(int x)
+{
+    switch (x)
+    {
+    case 4:
+        return 14;
+        break;
+    case 9:
+        return 26;
+        break;
+    case 12:
+        return 3;
+        break;
+    case 45:
+        return 22;
+        break;
+    case 19:
+        return 6;
+        break;
+    case 24:
+        return 20;
+        break;
+    case 28:
+        return 34;
+        break;
+    case 47:
+        return 39;
+        break;
+    case 49:
+        return 15;
+        break;
+    case 40:
+        return 7;
+        break;
+    default:
+        return 0;
+    }
+}
